dc9de3881117f54774475d96996904a026395d67
Eduardo San Martin Morote <posva13@gmail.com>
2020-12-21 10:57:06 +0100

    chore: add todo about ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 export { injectRouterMock, createProvide } from './injections'
 export { RouterMock, createRouterMock } from './router'
+
+// TODO: find how to do
+// import { RouterMock } from './router'
+// declare module '@vue/runtime-core' {
+//   export interface ComponentCustomProperties {
+//     /**
+//      * Router Mock instance used by the application.
+//      */
+//     $router: RouterMock
+//   }
+// }
