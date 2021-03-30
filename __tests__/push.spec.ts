19998867a16eadb033cf0a7e839dcd3990aa6f64
Eduardo San Martin Morote <posva13@gmail.com>
2021-03-30 10:49:02 +0200

    test: more

--- __tests__/push.spec.ts
+++ __tests__/push.spec.ts
@@ -11,11 +11,14 @@ describe('router.push mock', () => {
     const router = getRouter()
     // this is not defined
     await expect(router.push({ name: 'one' })).resolves.toBe(undefined)
+    // make sure the number of calls is just one and it isn't called internally
+    expect(router.push).toHaveBeenCalledTimes(1)
   })
 
   it('can be configured to throw on invalid routes', async () => {
     const router = createRouterMock({ noUndeclaredRoutes: true })
     // this is not defined
     expect(() => router.push({ name: 'one' })).toThrowError('No match')
+    expect(router.push).toHaveBeenCalledTimes(1)
   })
 })
