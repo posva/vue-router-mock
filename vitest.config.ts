e24f3a13a66ea280e0a2dbe42f84e417a8144ca7
Eduardo San Martin Morote <posva13@gmail.com>
2024-07-17 20:57:29 +0200

    test: up coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,13 @@ export default defineConfig({
   test: {
     setupFiles: ['./__tests__/setup.ts'],
     environment: 'happy-dom',
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'lcovonly', 'html'],
+      all: true,
+      include: ['src'],
+      exclude: ['src/index.ts', 'src/**/*.test-d.ts', 'src/testers'],
+    },
   },
 })
